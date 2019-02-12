d675e8ea9d16b18d2f78f52ac8b7fea932670208
Loris Leiva <loris.leiva@gmail.com>
2019-02-12 17:10:29 +0000

    :arrow_down: Remove lodash dependency
    Fixing security issue

--- gradients/index.js
+++ gradients/index.js
@@ -1,9 +1,9 @@
-const map = require('lodash/map')
-const isPlainObject = require('lodash/isPlainObject')
+const mapObject = (obj, fn) => Object.keys(obj).map(key => fn(obj[key], key, obj));
+const isPlainObject = val => !!val && typeof val === 'object' && val.constructor === Object;
 
 module.exports = function ({ gradients, variants }) {
     return function ({ addUtilities, e }) {
-        const utilities = map(gradients, (gradient, name) => {
+        const utilities = mapObject(gradients, (gradient, name) => {
 
             const type = isPlainObject(gradient) && gradient.hasOwnProperty('type') ? gradient.type : 'linear'
             const colors = isPlainObject(gradient) ? (gradient.colors || []) : gradient
