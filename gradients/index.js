a6edf7c106613dbd64f00f888c882326d88c04ad
Loris Leiva <loris.leiva@gmail.com>
2019-06-11 18:43:29 +0100

    :pencil: Update gradient plugin

--- gradients/index.js
+++ gradients/index.js
@@ -1,20 +1,20 @@
 const mapObject = (obj, fn) => Object.keys(obj).map(key => fn(obj[key], key, obj));
 const isPlainObject = val => !!val && typeof val === 'object' && val.constructor === Object;
 
-module.exports = function ({ gradients, variants }) {
-    return function ({ addUtilities, e }) {
-        const utilities = mapObject(gradients, (gradient, name) => {
+module.exports = function ({ addUtilities, e, theme, variants }) {
+    const gradients = theme('gradients', {})
+    const gradientVariants = variants('gradients', [])
+    
+    const utilities = mapObject(gradients, (gradient, name) => {
+        const type = isPlainObject(gradient) && gradient.hasOwnProperty('type') ? gradient.type : 'linear'
+        const colors = isPlainObject(gradient) ? (gradient.colors || []) : gradient
 
-            const type = isPlainObject(gradient) && gradient.hasOwnProperty('type') ? gradient.type : 'linear'
-            const colors = isPlainObject(gradient) ? (gradient.colors || []) : gradient
-
-            return {
-                [`.bg-${e(name)}`]: {
-                    backgroundImage: `${type}-gradient(${colors.join(', ')})`
-                }
+        return {
+            [`.bg-${e(name)}`]: {
+                backgroundImage: `${type}-gradient(${colors.join(', ')})`
             }
-        })
+        }
+    })
 
-        addUtilities(utilities, variants)
-    }
+    addUtilities(utilities, gradientVariants)
 }
